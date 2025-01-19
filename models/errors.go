9ca241b1228738ad47c6461f2b8a83dd4fb2948c
malijoe <malijoe0122@gmail.com>
2025-01-19 10:20:03 -0800

    correct regular expressions

--- models/errors.go
+++ models/errors.go
@@ -16,14 +16,11 @@ var (
 	ErrReceiptInvalid           = errors.New("invalid receipt")
 
 	// regex to validate retailer field
-	// provided pattern for retailer field '^[\\w\\s\\-&]+$' did not compile so the pattern was changed
-	// to match the one provided for the shortDescription field of the item object.
-	retailerRegex = regexp.MustCompile(`^[\\w\\s\\-]+$`)
+	retailerRegex = regexp.MustCompile(`^[\w\s\-&]+$`)
 	// regex to validate price formated strings.
 	priceRegex = regexp.MustCompile(`^\d+\.\d{2}$`)
 	// regex to validate the shortDescription field for Item objects.
-	// Accepts only the following characters: "w","s","-","\".
-	shortDescriptionRegex = regexp.MustCompile(`^[\\w\\s\\-]+$`)
+	shortDescriptionRegex = regexp.MustCompile(`^[\w\s\-]+$`)
 	// regex for catching all individual alphanumeric characters.
 	alphanumericRegex = regexp.MustCompile(`[\w\d]`)
 )
